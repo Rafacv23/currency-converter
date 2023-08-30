13ae368ab5af9cb74b50f896282998c522f619da
Rafa Canosa <rafacv23@gmail.com>
2023-08-30 17:58:31 +0200

    added more currencies

--- src/currency-info.js
+++ src/currency-info.js
@@ -12,7 +12,7 @@ const currencyInfo = [
     flag: "https://cdn-icons-png.flaticon.com/128/197/197484.png"
   },
   {
-    name: "libra",
+    name: "pound",
     short: "gbp",
     symbol: "£",
     flag: "https://cdn-icons-png.flaticon.com/128/323/323329.png"
@@ -22,6 +22,60 @@ const currencyInfo = [
     short: "jpy",
     symbol: "¥",
     flag: "https://cdn-icons-png.flaticon.com/128/197/197604.png"
+  },
+  {
+    name: "australian dollar",
+    short: "aud",
+    symbol: "$",
+    flag: "https://cdn-icons-png.flaticon.com/128/323/323367.png"
+  },
+  {
+    name: "canadian dollar",
+    short: "cad",
+    symbol: "$",
+    flag: "https://cdn-icons-png.flaticon.com/128/197/197430.png"
+  },
+  {
+    name: "swiss franc",
+    short: "chf",
+    symbol: "Fr",
+    flag: "https://cdn-icons-png.flaticon.com/128/197/197540.png"
+  },
+  {
+    name: "Chinese Yuan",
+    short: "cny",
+    symbol: "¥",
+    flag: "https://cdn-icons-png.flaticon.com/128/197/197375.png"
+  },
+  {
+    name: "Singapore dollar",
+    short: "sgd",
+    symbol: "$",
+    flag: "https://cdn-icons-png.flaticon.com/128/197/197496.png"
+  },
+  {
+    name: "Peso mexicano",
+    short: "mxn",
+    symbol: "$",
+    flag: "https://cdn-icons-png.flaticon.com/128/5372/5372848.png"
+  },
+  {
+    name: "Peso argentino",
+    short: "ars",
+    symbol: "$",
+    flag: "https://cdn-icons-png.flaticon.com/128/3909/3909430.png"
+  },
+  {
+    name: "Real brasileño",
+    short: "brl",
+    symbol: "R$",
+    flag: "https://cdn-icons-png.flaticon.com/128/197/197386.png"
+  },
+  {
+    name: "Rublo ruso",
+    short: "rub",
+    symbol: "₽",
+    flag: "https://cdn-icons-png.flaticon.com/128/197/197408.png"
   }
 ]
 
