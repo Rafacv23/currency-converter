ba7b0763501eb364bf8dc2f1cee0ffd630e528e1
Rafa Canosa <rafacv23@gmail.com>
2023-08-30 17:44:18 +0200

    Fix one problem with the currency-btn

--- src/components/currency-input-box-below.jsx
+++ src/components/currency-input-box-below.jsx
@@ -4,20 +4,20 @@ import Btn from "./btn"
 import "../styles/currency-input-box.css"
 
 const CurrencyInputBoxBelow = ({
-  setCurrencies,
   secondCurrencies,
   currencyData,
-  enableData,
-  changeCurrency,
   convertedAmount
 
 }) => {
-  const handleClick = (short) => {
+  // eslint-disable-next-line no-lone-blocks
+  { /*
+    *TODO: Add this button to change the second currency
+    const handleClick = (short) => {
     enableData()
     changeCurrency(short)
     console.log(short)
     setCurrencies(short)
-  }
+  */ }
 
   const selectedCurrency = currencyData.find((currency) => currency.short === secondCurrencies)
 
@@ -27,7 +27,7 @@ const CurrencyInputBoxBelow = ({
           ? (<div className="display">
               <Text text={selectedCurrency.symbol}></Text>
               <Text className="input" text={convertedAmount} />
-              <Btn className="currency-btn" text={selectedCurrency.short.toUpperCase()} onClick={handleClick}>
+              <Btn className="currency-btn" text={selectedCurrency.short.toUpperCase()}>
                 <img width="40px" key={selectedCurrency} src={selectedCurrency.flag} />
               </Btn>
             </div>)
